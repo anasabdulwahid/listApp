b8f7a5468ee16afaf783026b5e940bbd6a4d2c3a
Anas Abdul <anasabdulwahid@gmail.com>
2021-04-22 21:37:35 +0800

    Latest Update

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const names =['Anas', 'Jhon', 'Choo', 'David'];
+const names =['Anas', 'Jhon', 'Choo', 'David', 'kim', 'Brother'];
 
 export default function App() {
   return (
